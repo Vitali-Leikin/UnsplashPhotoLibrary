4e029a10d8916a6df4ffff8003c37b623c4dc79a
Vitali <vitali@Anastasiyas-MacBook-Air.local>
2024-12-10 11:16:45 +0100

    [ADDED] project

---
+++ UnsplashPhotoLibrary/UnsplashPhotoLibrary/FirstCollectionVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnsplashPhotoLibrary
+//
+//  Created by Vitali on 10/12/2024.
+//
+
+import UIKit
+
+class FirstCollectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
